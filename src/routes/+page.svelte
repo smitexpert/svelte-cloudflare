8dc9d7a991907162e025b5f4d742a80a68ff0fbb
Sujan Molla <smitexpert@gmail.com>
2024-04-14 23:52:24 +0600

    UPDATE | Add Socket to test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script>
+
+import {io} from "socket.io-client"
+
+
+const socket = io()
+
+socket.on('eventFromServer', (message) => {
+    console.log(message);
+})
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
